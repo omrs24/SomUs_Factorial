d43ad460b158415cb127849c5fedd89037fafaf0
orosalesg <omar.rosales@mbr.mx>
2023-03-28 18:08:49 -0600

    Agregar archivos de proyecto.

---
+++ SumUs_Factorial/Factorial.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumUs_Factorial
+{
+    internal class Factorial
+    {
+        int InputInt;
+        public Factorial(int num) 
+        {
+            this.InputInt = num;
+        }
+
+        public void GetFactorial()
+        {
+            // Variable para almacenar resultado de multiplicaciones
+            int result = 1;
+
+            // Comenzamos la salida
+            Console.Write($" {InputInt}! = ");
+
+            // Mostramos cada iteracion de InputInt hasta llegar a 1
+            for (int i = InputInt; i > 0; i--)
+            {
+                // Multiplicar por cada iteracion 
+                result *= i;
+
+                Console.Write(i);
+
+                // Al llegar a 1 mostramos el total de las multiplicaciones y salimos del loop
+                if (i == 1)
+                {
+                    Console.Write($" = { result }");
+                    break;
+                }
+
+                Console.Write(" x ");
+
+            }
+
+        }
+    }
+}
---
+++ SumUs_Factorial/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumUs_Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ingrese un numero para saber su factorial: ");
+
+            int result = GetInputInteger();
+
+            Factorial factorial = new Factorial(result);
+
+            factorial.GetFactorial();
+
+            // Para que no cierre el programa
+            Console.ReadLine();
+
+        }
+
+        public static int GetInputInteger() 
+        {
+            int inputint = 0;
+
+            // Validacion Int
+            while(!int.TryParse(Console.ReadLine(),out inputint));
+
+            return inputint; 
+        }
+    }
+}
